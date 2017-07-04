27ba4570b81a1331eba087bfd2144738cc4bb40c
Porscha07 <porscha.uga@gmail.com>
2017-07-03 22:56:44 -0400

    worked on styling home page

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,49 @@
+
 var express = require('express');
 var router = express.Router();
-var request = require('request')
-
+var request = require('request');
+// var jquery = require('jquery');
+var $ = require("jquery");
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function(req, res, next) {//home
+  res.render('index', { 
+  						
+  						title: 'Home',
+  						mainFrameSearch: "home" 
+  					});
 });
+
+router.get('/search', function(req, res, next) {
+  res.render('search.ejs', { 
+  						
+  						title: 'search',
+  						mainFrameSearch: "Search" 
+  					});
+});
+router.get('/reviews', function(req, res, next) {
+  res.render('index', { 
+  						
+  						title: 'reviews',
+  						mainFrameSearch: "Reviews"
+  					});
+});
+router.get('/events', function(req, res, next) {
+  res.render('index', { 
+  						
+  						title: 'events',
+  						mainFrameSearch: "Events"
+  					});
+});
+router.get('/specials', function(req, res, next) {
+  res.render('index', { 
+  						
+  						title: 'specials',
+  						mainFrameSearch: "Specials"
+  					});
+});
+
+
+
+
+
 module.exports = router;
---
+++ routes/scripts.js
@@ -0,0 +1,9 @@
+$(document).ready(function($) {
+  var options = {  videoId: 
+    'KX0Rf-OWLzo', 
+      start: 0, 
+      repeat: true
+  };  
+  
+  $('#wrapper').tubular(options); 
+}); 
